ec97dc89a152e3268a97519266b2e2fcfe92f94b
macstudent <macstudent@10.10.150.189>
2019-05-14 12:29:19 -0400

    last cpommit

--- test/selenium/testcase.java
+++ test/selenium/testcase.java
@@ -14,7 +14,26 @@ public class testcase {
 
 	@After
 	public void tearDown() throws Exception {
-	}
+	}System.setProperty("webdriver.chrome.driver", "/Users/parrot/Downloads/chromedriver");
+	WebDriver driver = new ChromeDriver();
+	
+	// go to the webpage
+	
+	driver.get("https://www.webdirectory.com/Animals/");
+	
+	// get all links for that section
+	List<WebElement> list = driver.findElements(By.xpath("/html/body/ul/li"));
+	
+	// test that the number of links matches
+	assertEquals(10, list.size());
+	
+	for (int i = 0; i < list.size(); i++) {
+		WebElement e = list.get(i);
+		// get the link
+		WebElement link = e.findElement(By.xpath("a"));
+		String url = link.getAttribute("href");
+		
+		System.out.println("Link #" + i + ": " + ur
 
 	@Test
 	public void test() {
